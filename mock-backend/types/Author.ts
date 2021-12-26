bdc7fe8d8ad7970a187b53cea0e50007922c60e4
Mario Budischek <mario.budischek@wienit.at>
2021-12-26 21:35:50 +0100

    global message styling

---
+++ mock-backend/types/Author.ts
@@ -0,0 +1,15 @@
+interface Author {
+  firstname: string;
+  lastname: string;
+  isPenName: boolean;
+  birthDate: Date;
+  birthLocation: string;
+  website: string;
+
+  note: string
+
+  //TODO other field ideas
+  // genres
+}
+
+export default Author;
---
+++ mock-backend/types/AuthorDataService.ts
@@ -0,0 +1,15 @@
+import Author from './Author';
+
+const initialAuthors: Author[] = [
+  {
+    firstname: "Brandon",
+    lastname: "Sanderson",
+    isPenName: false,
+    birthDate: new Date("1975-12-19"),
+    birthLocation: "Lincoln, Nebraska, The United States",
+    website: "http://brandonsanderson.com",
+    note: "Brandon Sanderson (born December 19, 1975) is an American author of epic fantasy and science fiction. He is best known for the Cosmere fictional universe, in which most of his fantasy novels, most notably the Mistborn series and The Stormlight Archive, are set. Outside of the Cosmere, he has written several young adult[a] and juvenile series including The Reckoners, the Skyward series, and the Alcatraz series. He is also known for finishing Robert Jordan's high fantasy series The Wheel of Time and has created several graphic novel fantasy series including the White Sand and Dark One.\n" +
+        "\n" +
+        "He created Sanderson's Laws of Magic and popularized the terms \"hard and soft magic systems\". In 2008, Sanderson started a podcast with author Dan Wells and cartoonist Howard Tayler called Writing Excuses, involving topics about creating genre writing and webcomics."
+  }
+];
