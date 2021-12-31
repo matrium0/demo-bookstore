05ab72caf41b227bb2a36ee6fcd259863e1732fe
Mario Budischek <mario.budischek@wienit.at>
2021-12-31 01:07:52 +0100

    added author edit page

--- angular/src/app/authors/author-edit/author-edit.component.ts
+++ angular/src/app/authors/author-edit/author-edit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Author from '../../../../../mock-backend/author/Author';
+import {findById} from '../../../../../mock-backend/author/AuthorMockService';
+import {ActivatedRoute} from '@angular/router';
+import {GlobalMessageService} from '../../core/global-message.service';
 
 @Component({
   selector: 'app-author-edit',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author-edit.component.scss']
 })
 export class AuthorEditComponent implements OnInit {
+  author?: Author;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private globalMessageService: GlobalMessageService) {
+  }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(
+        params => {
+          const id = params['id'];
+          if (id === "new") {
+            //TODO create new author
+            // author = new Author();
+
+          }else{
+            this.loadAuthor(id);
+          }
+        }
+    )
+
+  }
+
+  loadAuthor(id: number) {
+    findById(id).subscribe({
+      next: (author: Author) => {
+        console.log("findById", author);
+        this.author = author;
+      },
+      error: (error) => {
+        this.globalMessageService.setAlertMessage("danger", "Unable to load Authors: ", error);
+        console.log("error occured");
+      }
+    });
   }
 
 }
