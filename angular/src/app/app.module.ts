9999684c5d2c9a696206e627d1625d1848e9dab7
Mario Budischek <mario.budischek@wienit.at>
2022-01-07 16:26:59 +0100

    minor

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from '@core/core.module';
-import {MatLuxonDateModule} from '@angular/material-luxon-adapter';
 import {SharedModule} from '@shared/shared.module';
 
 
@@ -17,10 +15,9 @@ import {SharedModule} from '@shared/shared.module';
   imports: [
     CoreModule,
     SharedModule,
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
-//TODO move to core module?
   ],
 
   bootstrap: [AppComponent]
