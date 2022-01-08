45fd25bfde64daa9b6cf8f32856e75976a7cf364
Mario Budischek <mario.budischek@wienit.at>
2022-01-08 08:08:11 +0100

    author detail page wip

--- mock-backend/author/Author.ts
+++ mock-backend/author/Author.ts
@@ -5,13 +5,15 @@ interface Author extends Entity {
   firstname: string;
   lastname: string;
   penName: boolean;
+  fullRealName?: string;
   gender: "MALE" | "FEMALE" | "NON-BINARY";
   birthdate: DateTime;
   placeOfBirth: string;
   dateOfDeath?: DateTime;
   placeOfDeath?: string;
   website?: string;
-  note: string
+  note: string;
+  genre: string;
   foto: Blob;
 
   //TODO other field ideas
--- mock-backend/author/AuthorMockService.ts
+++ mock-backend/author/AuthorMockService.ts
@@ -13,6 +13,7 @@ const initialData: Author[] = [
     birthdate: DateTime.local(1975, 12, 19),
     placeOfBirth: "Lincoln, Nebraska, The United States",
     website: "http://brandonsanderson.com",
+    genre: "Science Fiction & Fantasy, Young Adult",
     note: "Brandon Sanderson (born December 19, 1975) is an American author of epic fantasy and science fiction. He is best known for the Cosmere fictional universe, in which most of his fantasy novels, most notably the Mistborn series and The Stormlight Archive, are set. Outside of the Cosmere, he has written several young adult[a] and juvenile series including The Reckoners, the Skyward series, and the Alcatraz series. He is also known for finishing Robert Jordan's high fantasy series The Wheel of Time and has created several graphic novel fantasy series including the White Sand and Dark One.\n" +
         "\n" +
         "He created Sanderson's Laws of Magic and popularized the terms \"hard and soft magic systems\". In 2008, Sanderson started a podcast with author Dan Wells and cartoonist Howard Tayler called Writing Excuses, involving topics about creating genre writing and webcomics.",
@@ -27,6 +28,7 @@ const initialData: Author[] = [
     birthdate: DateTime.local(1947, 9, 21),
     placeOfBirth: "Portland, Maine, The United States",
     website: "https://stephenking.com/",
+    genre: "Horror, Mystery, Literature & Fiction",
     note: "Stephen Edwin King (born September 21, 1947) is an American author of horror, supernatural fiction, suspense, crime, science-fiction, and fantasy novels. Described as the \"King of Horror\", a play on his surname and a reference to his high standing in pop culture,[2] his books have sold more than 350 million copies,[3] and many have been adapted into films, television series, miniseries, and comic books. King has published 63 novels, including seven under the pen name Richard Bachman, and five non-fiction books.[4] He has also written approximately 200 short stories, most of which have been published in book collections.",
     foto: dataURItoBlob()
   },
@@ -35,12 +37,14 @@ const initialData: Author[] = [
     firstname: "Anne",
     lastname: "Rice",
     gender: "FEMALE",
-    penName: false,
+    penName: true,
+    fullRealName: "Howard Allen Frances O'Brien",
     birthdate: DateTime.local(1941, 10, 4),
     placeOfBirth: "New Orleans, Louisiana, The United States",
     dateOfDeath: DateTime.local(2021, 12, 1),
     placeOfDeath: "Rancho Mirage, California, United States",
     website: "http://annerice.com",
+    genre: "Horror, Historical Fiction, Romance",
     note: "Anne Rice[1] (born Howard Allen Frances O'Brien; October 4, 1941 – December 11, 2021) was an American author of gothic fiction, erotic literature, and Christian literature. She was best known for her series of novels The Vampire Chronicles. Books from The Vampire Chronicles were the subject of two film adaptations—Interview with the Vampire (1994) and Queen of the Damned (2002).",
     foto: dataURItoBlob()
   },
@@ -50,8 +54,10 @@ const initialData: Author[] = [
     lastname: "Bachman",
     gender: "MALE",
     penName: true,
+    fullRealName: "Stephen King",
     birthdate: DateTime.local(1947, 9, 21),
     placeOfBirth: "Portland, Maine, The United States",
+    genre: "Crime, Horror",
     note: "Stephen Edwin King (born September 21, 1947) is an American author of horror, supernatural fiction, suspense, crime, science-fiction, and fantasy novels. Described as the \"King of Horror\", a play on his surname and a reference to his high standing in pop culture,[2] his books have sold more than 350 million copies,[3] and many have been adapted into films, television series, miniseries, and comic books. King has published 63 novels, including seven under the pen name Richard Bachman, and five non-fiction books.[4] He has also written approximately 200 short stories, most of which have been published in book collections.",
     foto: dataURItoBlob()
   },
