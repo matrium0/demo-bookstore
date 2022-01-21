436b4d11b6355d83d98ab85d87a3e1de800e60c6
mario.budischek <mario.budischek@gmail.com>
2022-01-21 19:56:41 +0100

    general styling

--- react/src/features/home.tsx
+++ react/src/features/home.tsx
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import {GlobalMessageContext} from '../shared/GlobalMessageContext';
+import {Paper} from '@mui/material';
 
 const Home = () => {
   const globalMessageContext = useContext(GlobalMessageContext);
@@ -18,32 +19,30 @@ const Home = () => {
   }
 
   return (
-      <div className="app-col mat-elevation-z8">
-        <div className="p-2 ms-lg-3 title-row d-flex align-items-center">
-          <div className="d-flex align-items-center justify-content-between flex-wrap">
-            <h1>Home</h1>
+      <div className="comp-wrapper">
+        <Paper elevation={8} className="app-col">
+          <div className="p-2 ms-lg-3 title-row d-flex align-items-center">
+            <div className="d-flex align-items-center justify-content-between flex-wrap">
+              <h1>Home</h1>
+            </div>
           </div>
-        </div>
-        <div className="pb-3" style={{borderTop: '2px solid gray'}}>
-          <p className="mx-2 mx-lg-5 mt-0 mt-3 fs-5">
-            Welcome to our <strong>Demo Bookstore</strong>. This is a just a frontend demonstration with no backend or persistent database.
-            Feel free to edit, add or delete anything you like - the data is kept in <strong>your browsers memory</strong> only and will not
-            be shared with others.
-            If you leave the page all changes are lost!
-            <br/>
-            <br/>
-            So don't be scared - go crazy :)
-          </p>
-          <p className="mx-2 mx-lg-5 mt-0 mt-3 fs-6">
-            Since this is a playground different pages are implemented differently:
-            <br/>
-            LibraryComponent and YourBooksListComponent are fully "reactive" by embracing the full power of RXJS (e.g. combining streams
-            with combineLatest) and
-            async pipe
-            <br/>
-            Other smart components may store data directly or use a mix of the two.
-          </p>
-        </div>
+          <div className="pb-3" style={{borderTop: '2px solid gray'}}>
+            <p className="mx-2 mx-lg-5 mt-0 mt-3 fs-5">
+              Welcome to our <strong>Demo Bookstore</strong>. This is a just a frontend demonstration with no backend or persistent
+              database.
+              Feel free to edit, add or delete anything you like - the data is kept in <strong>your browsers memory</strong> only and will
+              not
+              be shared with others.
+              If you leave the page all changes are lost!
+              <br/>
+              <br/>
+              So don't be scared - go crazy :)
+            </p>
+            <p className="mx-2 mx-lg-5 mt-0 mt-3 fs-6">
+              {/*TODO some text about technical details in react*/}
+            </p>
+          </div>
+        </Paper>
       </div>
   );
 };
