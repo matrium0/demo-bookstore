5524e3d6ce9d1c72c31a59be5f0a186c74c36adf
mario.budischek <mario.budischek@gmail.com>
2022-01-29 01:23:01 +0100

    book detail dialog

--- react/src/features/home.tsx
+++ react/src/features/home.tsx
@@ -1,22 +1,7 @@
-import React, {useContext} from 'react';
-import {GlobalMessageContext} from '../shared/GlobalMessageContext';
+import React, {memo} from 'react';
 import {Paper} from '@mui/material';
 
 const Home = () => {
-  const globalMessageContext = useContext(GlobalMessageContext);
-  console.log("Home");
-
-  function setDummyMessage() {
-    console.log("settingy dummy message");
-    //TODO add default values for optional fields
-    //TODO use undefined instead of null so that no value has to be set
-    globalMessageContext.setMessage({message: "a bright new message of medium length", severity: "info"})
-  }
-
-  function clearMessage() {
-    console.log("clearMessage");
-    globalMessageContext.setMessage({message: "", severity: "info"})
-  }
 
   return (
     <div className="comp-wrapper">
@@ -24,8 +9,6 @@ const Home = () => {
         <div className="p-2 ms-lg-3 title-row d-flex align-items-center">
           <div className="d-flex align-items-center justify-content-between flex-wrap">
             <h1>Home</h1>
-            <button onClick={() => setDummyMessage()} className="mx-5 btn btn-primary">set dummy message</button>
-            <button onClick={() => clearMessage()} className="mx-5 btn btn-primary">clear message</button>
           </div>
         </div>
         <div className="pb-3" style={{borderTop: '2px solid gray'}}>
@@ -49,4 +32,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default memo(Home);
