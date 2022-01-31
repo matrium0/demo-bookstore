db063d277e2cac8dd17e5391d2c000bf46f5e96e
mario.budischek <mario.budischek@gmail.com>
2022-01-31 23:08:17 +0100

    removed changedetection onpush from reactive form components

--- angular/src/app/shared/reactive-validation-display/reactive-validation-display.component.ts
+++ angular/src/app/shared/reactive-validation-display/reactive-validation-display.component.ts
@@ -1,11 +1,10 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-validation-display',
   templateUrl: './reactive-validation-display.component.html',
-  styleUrls: ['./reactive-validation-display.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./reactive-validation-display.component.scss']
 })
 export class ReactiveValidationDisplayComponent {
   @Input() control?: AbstractControl | null;
