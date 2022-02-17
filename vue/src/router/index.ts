482d0ad938dda6b57a8e7a0704c3aedcbbc383e5
mario.budischek <mario.budischek@gmail.com>
2022-02-17 08:11:54 +0100

    fixes home redirect

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -8,6 +8,7 @@ import AuthorDetail from '../components/authors/AuthorDetail.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {path: '/', redirect: "/home"},
     {path: '/home', name: 'home', component: HomeView},
 
     {path: "/author", name: 'author-list', component: AuthorList},
