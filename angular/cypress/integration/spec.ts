443e0580b02ee6f0b393d0071c2d8fc17ae9e9d4
mario.budischek <mario.budischek@gmail.com>
2022-04-15 17:18:25 +0200

    added cypress

--- angular/cypress/integration/spec.ts
+++ angular/cypress/integration/spec.ts
@@ -1,7 +1,18 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
-  })
+    cy.visit('/');
+    cy.contains('Welcome');
+    cy.contains('Welcome to');
+  });
+  it('Navigate and display Author Page', () => {
+    cy.visit('/author');
+    cy.get("h1").contains("Authors");
+  });
+
+  it('Navigate and display Author Page', () => {
+    cy.visit('/');
+    cy.get('a').contains('Author').click();
+    cy.get("h1").contains("Authors");
+  });
 })
+
